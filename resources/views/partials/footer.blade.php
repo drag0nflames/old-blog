d5b61095bb5d5b8728e03b14f835bbb91fce2b10
bikky11 <bicky.bhujel@gmail.com>
2016-12-06 13:59:57 +0545

    edited

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,38 +1,40 @@
-<div class="footer panel-footer navbar-fixed-bottom">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-2">
-                <h6>Copyright &copy; <?php echo "2013-".date('Y');?></h6>
-            </div><!-- ./end col-sm-2-->
+<footer>
+    <div class="footer-style panel-footer navbar-fixed-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <h6>Copyright &copy; <?php echo "2013-".date('Y');?></h6>
+                </div><!-- ./end col-sm-2-->
 
-            <div class="col-sm-4">
-                <h6>About us </h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-            </div><!-- ./end col-sm-4-->
+                <div class="col-sm-4">
+                    <h6>About us </h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+                </div><!-- ./end col-sm-4-->
 
-            <div class="col-sm-2">
-                <h6>Navigation</h6>
-                <ul class="list-unstyled">
-                    <li><a href="www.facebook.com">Home</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Links</a></li>
-                </ul>
-            </div><!-- ./end col-sm-2-->
+                <div class="col-sm-2">
+                    <h6>Navigation</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="www.facebook.com">Home</a></li>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Links</a></li>
+                    </ul>
+                </div><!-- ./end col-sm-2-->
 
-            <div class="col-sm-2">
-                <h6>Follow Us:</h6>
-                <ul class="list-unstyled">
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Tumblr</a></li>
-                    <li><a href="#">Google Plus</a></li>
-                </ul>
-            </div><!-- ./end col-sm-2-->
+                <div class="col-sm-2">
+                    <h6>Follow Us:</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Tumblr</a></li>
+                        <li><a href="#">Google Plus</a></li>
+                    </ul>
+                </div><!-- ./end col-sm-2-->
 
-            <div class="col-sm-2">
-                <h6>Coded with <i class="fa fa-heart" aria-hidden="true"></i> by Bikey Bhujel</h6>
-            </div><!-- ./end col-sm-2-->
-        </div><!-- ./end row-->
-    </div><!-- ./end container-->
-</div><!-- ./ end footer-->
+                <div class="col-sm-2">
+                    <h6>Coded with <i class="fa fa-heart" aria-hidden="true"></i> by Bikey Bhujel</h6>
+                </div><!-- ./end col-sm-2-->
+            </div><!-- ./end row-->
+        </div><!-- ./end panel-->
+    </div><!-- ./ end of container-->
+</footer><!-- end of footer -->
