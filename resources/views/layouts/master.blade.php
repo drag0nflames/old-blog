49700fb6ecf3c9f28da8a54509e40269cb485995
bikky11 <bicky.bhujel@gmail.com>
2016-12-06 11:43:34 +0545

    editing tags  for master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     @include('partials.header')
     <div class="container">
         @yield('content')
-        </div>
+    </div>
     @include('partials.footer')
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified JavaScript -->
