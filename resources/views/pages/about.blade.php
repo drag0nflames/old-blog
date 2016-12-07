19111942524630d2d7013a613dd5f390e191486f
bikky11 <bicky.bhujel@gmail.com>
2016-12-07 09:46:51 +0545

    edited contents

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!</p>
+@endsection
