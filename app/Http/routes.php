423c0dcde4952cf973a172d6e2d3cd9b80c3ecd6
bikky11 <bicky.bhujel@gmail.com>
2016-12-07 09:49:20 +0545

    commited unversioned files

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('pages.contact');
 });
+
+Route::get('/about',function (){
+    return view('pages.about');
+});
