05e5e8479de2ad7ef37c3be915214b19ef782030
bikky11 <bicky.bhujel@gmail.com>
2016-12-08 15:26:34 +0545

    added protected fillable

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed id
+ */
 class Post extends Model
 {
-    //
+    protected $fillable = array('title', 'body');
 }
