2e7dca72e6cd6f53a5b668c7e53ff6a30a551bb6
bikky11 <bicky.bhujel@gmail.com>
2016-12-09 14:47:19 +0545

    created method to  extract data from database and pass to the view

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,6 +10,10 @@ use App\Http\Requests;
 class BlogController extends Controller
 {
     public function getSingle($slug){
-        return $slug;
+        //fetch from the database based on slug
+        $post=Post::where('slug', "=", $slug)->get();
+
+        //return the view and pass in the Post object
+        return view('blog.single')->withPost($post);
     }
 }
