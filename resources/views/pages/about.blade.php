79cb0acb720bca7dd6e4e9f0d1026de59d89c7af
bikky11 <bicky.bhujel@gmail.com>
2016-12-13 11:12:36 +0545

    edited

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,34 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>About Page</h1>
+    <h1 align="center">About Page</h1>
+    <hr>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
         quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam! . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
         ullam!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequuntur eum ipsam, itaque minima
+        molestias nemo nobis non placeat quas quasi quos, repellendus rerum sequi soluta suscipit tenetur vel velit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur culpa, enim ipsam ipsum
+        labore laborum, maiores nesciunt odit quasi qui quis quo recusandae sunt. Illum iste magnam temporibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+        ullam!</p>
+
 @endsection
