11fe2e544e0ea1eb3ad28987a9259b46d1b7f7c0
bikky11 <bicky.bhujel@gmail.com>
2016-12-13 11:14:32 +0545

    indent

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,34 +1,39 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1 align="center">About Page</h1>
-    <hr>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam! . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequuntur eum ipsam, itaque minima
-        molestias nemo nobis non placeat quas quasi quos, repellendus rerum sequi soluta suscipit tenetur vel velit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur culpa, enim ipsam ipsum
-        labore laborum, maiores nesciunt odit quasi qui quis quo recusandae sunt. Illum iste magnam temporibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
-        quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
-        ullam!</p>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+          <h1 align="center">About Page</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam! . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequuntur eum ipsam, itaque minima
+              molestias nemo nobis non placeat quas quasi quos, repellendus rerum sequi soluta suscipit tenetur vel velit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur culpa, enim ipsam ipsum
+              labore laborum, maiores nesciunt odit quasi qui quis quo recusandae sunt. Illum iste magnam temporibus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab assumenda beatae, dolor doloribus eligendi molestias
+              quae quod recusandae repudiandae saepe. Ab ad aspernatur cum facere laboriosam molestias, officiis quisquam
+              ullam!</p>
+      </div><!--end of col-md-10-->
+    </div><!-- end of row-->
+
 
 @endsection
