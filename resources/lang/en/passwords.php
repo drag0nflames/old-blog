c504efa6a94eac760b07bac8ca085f54ca779755
bikky11 <bicky.bhujel@gmail.com>
2016-12-13 09:32:38 +0545

    edited

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -8,12 +8,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
+    | that are given by the passwords broker for a passwords update attempt
+    | has failed, such as for an invalid token or invalid new passwords.
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'password' => 'Password must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
     'sent' => 'We have e-mailed your password reset link!',
     'token' => 'This password reset token is invalid.',
