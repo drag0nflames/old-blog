7b748ba4ea1736eb5e46cd7b7f016a6cab710d1b
bikky11 <bicky.bhujel@gmail.com>
2016-12-13 09:32:51 +0545

    edited

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -35,7 +35,7 @@ return [
     'digits'               => 'The :attribute must be :digits digits.',
     'digits_between'       => 'The :attribute must be between :min and :max digits.',
     'distinct'             => 'The :attribute field has a duplicate value.',
-    'email'                => 'The :attribute must be a valid email address.',
+    'emails'                => 'The :attribute must be a valid emails address.',
     'exists'               => 'The selected :attribute is invalid.',
     'filled'               => 'The :attribute field is required.',
     'image'                => 'The :attribute must be an image.',
@@ -104,7 +104,7 @@ return [
     |
     | The following language lines are used to swap attribute place-holders
     | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
+    | of "emails". This simply helps us make messages a little cleaner.
     |
     */
 
